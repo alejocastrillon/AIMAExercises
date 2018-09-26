f5914c0ff09d92c06dbea52ee4bcf34e7610b2c0
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2018-09-26 15:13:57 -0500

    Solucionado expresión algebraica

--- algebraexpr.py
+++ algebraexpr.py
@@ -4,22 +4,16 @@ from deap import base
 from deap import tools
 import random
 import numpy
-NUMBER_GOAL = 22.0
+NUMBER_GOAL = 50
 
 def validateExp(individual):
     for i in range(0, len(individual)):
         if i % 2== 0:
             if type(individual[i]) is str:
-                print(individual)
-                print("False")
                 return False
         else:
             if type(individual[i]) is int:
-                print(individual)
-                print("False")
                 return False
-    print(individual)
-    print("True")
     return True
 
 #Se evaluan los individuos
@@ -35,9 +29,12 @@ def evaluateInd(individual):
                 val = val * individual[i + 1]
             elif individual[i] == "/":
                 val = val / individual[i + 1]
-        peso = val -NUMBER_GOAL
-        print(peso)
-        return peso, #individual
+        peso = val - NUMBER_GOAL
+        if peso == 0:
+            print("Solución Optima: ", individual, peso)
+        else:
+            print(individual, peso)
+        return abs(peso), #individual
     else:
         return NUMBER_GOAL,
 
@@ -62,30 +59,29 @@ def generateIndivual():
 
 #print(generateIndivual())
 
-creator.create('FitnessMin', base.Fitness, weights=(-1.0, ))
+creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
 creator.create('Individual', list , fitness = creator.FitnessMin)
 toolbox = base.Toolbox()
 toolbox.register('individual', tools.initIterate, creator.Individual, generateIndivual)
 #ind1 = toolbox.individual()
 toolbox.register('population', tools.initRepeat, list, toolbox.individual)
 toolbox.register('mate', tools.cxOnePoint)
-toolbox.register('mutate', tools.mutShuffleIndexes, indpb = 0.2)
-toolbox.register('select', tools.selTournament, tournsize = 5)
+toolbox.register('mutate', tools.mutShuffleIndexes, indpb = 0.5)
+toolbox.register('select', tools.selTournament, tournsize = 4)
 toolbox.register('evaluate', evaluateInd)
 
 def main():
-    pop = toolbox.population(n = 20)
-    print(pop)
+    pop = toolbox.population(n = 200)
     hof = tools.HallOfFame(4)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
-    algorithms.eaSimple(pop, toolbox, 0.7, 0.2, 10, stats = stats, halloffame = hof)
+    algorithms.eaSimple(pop, toolbox, 0.02, 0.2, 100, stats = stats, halloffame = hof)
     return pop
 
 '''data = generateIndivual(10)
 print(data)
 print(evaluate(data))'''
-print(main())
+main()
