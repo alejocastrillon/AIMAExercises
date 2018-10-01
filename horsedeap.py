536941b42d3265eed6e8acbb21b6430d9c379444
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2018-10-01 12:00:11 -0500

    Implementación Total de Caballos Ajedrez y del TSP

--- horsedeap.py
+++ horsedeap.py
@@ -3,13 +3,14 @@ from deap import creator
 from deap import base
 from deap import tools
 import random
+import numpy
 
 def generateIndividual():
     individual = list()
     for y in range(0, 8):
         individual.append(list())
         for x in range(0,8):
-            if random.randint(0, 10) == 10:
+            if random.randint(0, 50) == 3:
                 individual[y].append(1)
             else:
                 individual[y].append(0)
@@ -23,7 +24,7 @@ def createMatrizEval():
             e[x].append(0)
     return e
 
-def evaluateIndividual(individual):
+def generateMatrizEvaluacion(individual):
     evalu = createMatrizEval()
     for y in range(0,8):
         for x in range(0,8):
@@ -46,14 +47,52 @@ def evaluateIndividual(individual):
                     evalu[y + 1][x - 2] = 2
                 if (y + 1) < 8 and (y + 1) >= 0 and (x + 2) < 8 and (x + 2) >= 0 and evalu[y + 1][x + 2] == 0:
                     evalu[y + 1][x + 2] = 2
-
     return evalu
 
-d = generateIndividual()
+def evaluateIndividual(individual):
+    e = generateMatrizEvaluacion(individual)
+    cantidadAtaques = 0
+    cantidadCaballos = 0
+    cantidadBlancos = 0
+    for y in e:
+        for x in y:
+            if x == 1:
+                cantidadCaballos += 1
+            elif x == 2:
+                cantidadAtaques += 1
+            else:
+                cantidadBlancos += 1
+    return abs((cantidadAtaques+cantidadCaballos)/64), 
+    #return (cantidadCaballos/cantidadAtaques) + cantidadBlancos
+
+'''d = generateIndividual()
 for x in d:
     print(x)
 
 print("Evaluacion: ")
-ev = evaluateIndividual(d)
-for x in ev:
-    print(x)
+ev = evalu'''
+
+creator.create('FitnessMin', base.Fitness, weights = (1.0, ))
+creator.create('Individual', list, fitness = creator.FitnessMin)
+toolbox = base.Toolbox()
+toolbox.register('individual', tools.initIterate, creator.Individual, generateIndividual)
+toolbox.register('population', tools.initRepeat, list, toolbox.individual)
+toolbox.register('mate', tools.cxOnePoint)
+toolbox.register('mutate', tools.mutShuffleIndexes, indpb = 0.2)
+toolbox.register('select', tools.selTournament, tournsize = 4)
+toolbox.register('evaluate', evaluateIndividual)
+
+
+def main():
+    pop = toolbox.population(n = 2000)
+    hof = tools.HallOfFame(4)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+    algorithms.eaSimple(pop, toolbox, 0.02, 0.2, 50, stats = stats, halloffame = hof)
+    print("Hall of fame: ", hof[0])
+    print("Evaluacion: ", generateMatrizEvaluacion(hof[0]))
+
+main()
